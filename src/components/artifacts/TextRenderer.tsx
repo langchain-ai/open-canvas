d528d2c87b8e3c378e3262691e69504f9e9f17a3
liuhuapiaoyuan <278780765@qq.com>
2024-10-19 10:34:13 +0800

    feat: Update markdown editor to render md live in editor

--- src/components/artifacts/TextRenderer.tsx
+++ src/components/artifacts/TextRenderer.tsx
@@ -1,10 +1,12 @@
 import { Artifact } from "@/types";
-import MDEditor from "@uiw/react-md-editor";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useEffect } from "react";
 import { cleanContent } from "@/lib/normalize_string";
 
-import styles from "./TextRenderer.module.css";
-
+import "@blocknote/core/fonts/inter.css";
+import { getDefaultReactSlashMenuItems, SuggestionMenuController, useCreateBlockNote } from "@blocknote/react";
+import { BlockNoteView } from "@blocknote/shadcn";
+import "@blocknote/shadcn/style.css";
+ 
 export interface TextRenderer {
   artifact: Artifact;
   isEditing: boolean;
@@ -13,20 +15,42 @@ export interface TextRenderer {
 }
 
 export function TextRenderer(props: TextRenderer) {
+  const editor = useCreateBlockNote({});
+
+  const onChange = async () => {
+    const markdown = await editor.blocksToMarkdownLossy(editor.document);
+    props.setArtifactContent(props.artifact.id, cleanContent(markdown));
+  };
+
+  useEffect(() => {
+    (async () => {
+      const blocks = await editor.tryParseMarkdownToBlocks(
+        props.artifact.content
+      );
+      editor.replaceBlocks(editor.document, blocks);
+    })();
+  }, [props.artifact.content, editor]);
+
   return (
     <div
-      className="w-full h-full mt-2 flex flex-col border-[1px] border-gray-200 rounded-2xl overflow-hidden"
+      className="w-full h-full mt-2 flex flex-col border-[1px] border-gray-200 rounded-2xl overflow-hidden py-5"
       data-color-mode="light"
     >
-      <MDEditor
-        preview={props.isEditing ? "edit" : "preview"}
-        hideToolbar
-        visibleDragbar={false}
-        value={cleanContent(props.artifact.content)}
-        onChange={(v) => props.setArtifactContent(props.artifact.id, v || "")}
-        className={`min-h-full border-none ${styles.mdEditorCustom} ${styles.fullHeightTextArea} ${styles.lightModeOnly}`}
-        height="100%"
-      />
+      <BlockNoteView
+        slashMenu={false}
+        editable={props.isEditing}
+        onChange={onChange}
+        editor={editor}
+      >
+        <SuggestionMenuController
+          getItems={async () =>
+            getDefaultReactSlashMenuItems(editor).filter(
+              (z) => z.group !== "Media"
+            )
+          }
+          triggerCharacter={"/"}
+        />
+      </BlockNoteView>
     </div>
   );
 }
