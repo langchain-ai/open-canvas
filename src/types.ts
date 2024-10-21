d29cdbb8d654d76785ee1f0d97c0056873bbc462
bracesproul <braceasproul@gmail.com>
2024-10-20 18:00:30 -0700

    feat: Custom quick actions

--- src/types.ts
+++ src/types.ts
@@ -82,3 +82,31 @@ export interface Reflections {
    */
   content: string[];
 }
+
+export interface CustomQuickAction {
+  /**
+   * A UUID for the quick action. Used to identify the quick action.
+   */
+  id: string;
+  /**
+   * The title of the quick action. Used in the UI
+   * to display the quick action.
+   */
+  title: string;
+  /**
+   * The prompt to use when the quick action is invoked.
+   */
+  prompt: string;
+  /**
+   * Whether or not to include the user's reflections in the prompt.
+   */
+  includeReflections: boolean;
+  /**
+   * Whether or not to include the default prefix in the prompt.
+   */
+  includePrefix: boolean;
+  /**
+   * Whether or not to include the last 5 (or less) messages in the prompt.
+   */
+  includeRecentHistory: boolean;
+}
