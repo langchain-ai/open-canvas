ca0c7fb2c5cbb8f935b84f3f70c9359d8fc9874e
Pratham271 <pratham19csu221@ncuindia.edu>
2025-05-28 06:35:28 +0530

    uncommented the middleware logic again

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -2,7 +2,7 @@ import { type NextRequest } from "next/server";
 import { updateSession } from "@/lib/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  // return await updateSession(request);
+  return await updateSession(request);
 }
 
 export const config = {
